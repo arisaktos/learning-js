e030f4b5f2d1d7a9ed4ff632bfd7d83f468474e0
Alicja <arisusaktos@gmail.com>
2017-10-17 22:43:16 +0200

    stuff on strings

---
+++ strings/js/main.js
@@ -0,0 +1,12 @@
+function reverseText(parametr) {
+    return parametr.split("").reverse().join("");
+}
+
+console.log(reverseText('Unicorns'));
+
+
+function arrageLetters(parametr) {
+    return parametr.split("").sort().join("");
+}
+
+console.log(arrageLetters('Unicorns'));
